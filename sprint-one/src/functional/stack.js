2cea7081cc1fab09771de0ce913861c8b1a46acd
Kent Shepard <kentshepard7@gmail.com>
2017-08-23 18:12:19 -0400

    Pass the first three tests for functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,21 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
+  var size = 0;
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
