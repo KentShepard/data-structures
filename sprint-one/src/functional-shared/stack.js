eac52f4e9b0507b8d345721cf221fdbf16ecc103
Kent Shepard <kentshepard7@gmail.com>
2017-08-23 22:38:29 -0400

    Set up functional shared version of stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+  _.extend(obj, stackMethods)
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
 
+}
+
+stackMethods.pop = function() {
+
+}
+
+stackMethods.size = function() {
+
+}
