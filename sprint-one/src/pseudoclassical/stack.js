e99897b40f0ddbfa309fa63e4761ea71bf0421f8
Kent Shepard <kentshepard7@gmail.com>
2017-08-24 13:43:04 -0400

    Pass all tests for pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -4,13 +4,20 @@ var Stack = function() {
 };
 
 Stack.prototype.push = function(value) {
-
+  this.count++;
+  this.storage[this.count] = value;
 };
 
 Stack.prototype.pop = function() {
+  var deleted = this.storage[this.count];
+  delete this.storage[this.count];
 
+  if (this.count > 0) {
+    this.count--;
+  }
+  return deleted
 };
 
 Stack.prototype.size = function() {
-
+  return this.count;
 };
