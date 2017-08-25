c2abd682e2c4eef43139a43bc884e69f71d18d8e
Kent Shepard <kentshepard7@gmail.com>
2017-08-25 19:43:29 -0400

    Pass all provided tests for tree.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -60,3 +60,6 @@ var Node = function(value) {
   return node;
 };
 
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,12 +13,30 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, currentNode) {
+  var targetFound = false;
+  currentNode = currentNode || this;
+  if (currentNode.constructor === Array) {
+    for (var i = 0; i < currentNode.length; i++) {
+      targetFound = this.contains(target, currentNode[i]);
+      if (targetFound) {
+        return targetFound;
+      }
+    }
+  }
+
+  if (currentNode.constructor === Object) {
+    if (currentNode.value === target) {
+      targetFound = true;
+    } else if (currentNode.children.length > 0) {
+      targetFound = this.contains(target, currentNode.children);
+    }
+  }
+  return targetFound;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
