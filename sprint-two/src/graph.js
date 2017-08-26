e20d730de03dcbfb7798c7a1fe5f1a0936a53d8a
Kent Shepard <kentshepard7@gmail.com>
2017-08-25 23:11:30 -0400

    Implement removeNode and pass the provided test

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -14,14 +14,25 @@ Graph.prototype.addNode = function(node) {
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return _.contains(this.nodes, node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var index = this.nodes.indexOf(node);
+  this.nodes.splice(index, 1);
+  var array = this.edges[node];
+  console.log(array);
+  for (var i = 0; i < array.length; i++) {
+    var index = this.edges[array[i]].indexOf(node);
+    this.edges[array[i]].splice(index, 1);
+  }
+  this.edges[node] = [];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return _.contains(this.edges[fromNode], toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
@@ -32,6 +43,7 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
