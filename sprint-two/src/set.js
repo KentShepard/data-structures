a1d6f7b38f8a4fbd6f34a5630fe7682c4eaf8a59
Kent Shepard <kentshepard7@gmail.com>
2017-08-25 23:56:09 -0400

    Pass all provided tests for Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
+  set._size = 0;
+
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._size++;
+  this._storage[this._size] = item;
 };
 
 setPrototype.contains = function(item) {
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
+  var storage = this._storage;
+  _.each(storage, function(value, key) {
+    if (value === item) {
+      delete storage[key]
+    }
+  });
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
